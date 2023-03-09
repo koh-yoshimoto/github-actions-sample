bacafee505ca7ecd430f769f748e26ad35d41fd9
koh-yoshimoto <yoshimoto.koshiro@gmail.com>
2023-03-09 23:26:14 +0900

    update go lambda function

--- api/main.go
+++ api/main.go
@@ -2,21 +2,33 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"strings"
 
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-type MyEvent struct {
+type Payload struct {
+	Body string `json:"body"`
+}
+
+type Request struct {
 	Name string `json:"name"`
 }
 
-type MyResponse struct {
+type Response struct {
 	Message string `json:"message"`
 }
 
-func HandleRequest(ctx context.Context, event MyEvent) (MyResponse, error) {
-	return MyResponse{Message: fmt.Sprintf("Hello %s!", event.Name)}, nil
+func HandleRequest(ctx context.Context, payload Payload) (Response, error) {
+	var req Request
+	err := json.NewDecoder(strings.NewReader(payload.Body)).Decode(&req)
+	if err != nil {
+		return Response{Message: "lambda function failed"}, err
+	}
+
+	return Response{Message: fmt.Sprintf("Hello %s from Lambda!", req.Name)}, nil
 }
 
 func main() {
